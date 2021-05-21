78b0cc0bb4a5ae5915fcbb6f14c37bb77484bca8
Sally Perez <68660009+SeattleSal@users.noreply.github.com>
2021-05-21 09:01:15 -0700

    updated about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,13 +15,7 @@ function About() {
           />
           <div className="container">
             <p className="mt-1">
-              Web developer with strong problem solving mindset and passion to
-              continuously learn new skills. Recently completed a Certificate in
-              Full Stack Web Development from the University of Washington.
-            </p>
-            <p className="mt-1">
-              Skills include JavaScript, ReactJS, jQuery, NodeJS, mySQL,
-              Mongoose, HTML5, CSS.
+            Full Stack Developer with strong problem solving mindset and a passion to continuously learn. Recently completed a Certificate in Full Stack Web Development from the University of Washington and have skills in JavaScript, React, Mongoose, MySQL, HTML and CSS. I also have a degree in Computer Science. My experience as a Technology Auditor showcases my ability to continuously learn and apply critical thinking to new areas of Technology and processes. I’m excited to move beyond the focus on analysis to creating valuable solutions through team collaboration and problem solving.
             </p>
           </div>
         </div>
