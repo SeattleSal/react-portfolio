095f48d6fa2fcb537ff26a176542f1fe78da5df7
Sally Perez <68660009+SeattleSal@users.noreply.github.com>
2021-09-05 13:42:13 -0700

    updated contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,14 +9,6 @@ function Contact() {
     <Container fluid style={{ marginTop: "1rem" }}>
       <Col className="ml-10">
         <Row style={{ margin: "1rem" }} >
-          <a href="mailto:seattlesally1@gmail.com">
-            <h4>
-              <i className="fas fa-envelope-square fa-3x mr-4"></i>
-              seattlesally1@gmail.com
-            </h4>
-          </a>
-        </Row>
-        <Row style={{ margin: "1rem" }} >
           <a href="https://github.com/SeattleSal/">
             <h4>
               <i className="fab fa-github-square fa-3x mr-4"></i>/SeattleSal
@@ -31,9 +23,17 @@ function Contact() {
           </a>
         </Row>
         <Row style={{ margin: "1rem" }} >
+          <a href="mailto:seattlesally1@gmail.com">
+            <h4>
+              <i className="fas fa-envelope-square fa-3x mr-4"></i>
+              sally.perez@gmail.com
+            </h4>
+          </a>
+        </Row>
+        <Row style={{ margin: "1rem" }} >
           <a href={resume} download>
             <h4>
-              <i className="fa fa-file fa-3x mr-4"></i>Download Resume
+              <i className="fa fa-file fa-3x mr-4"></i>Resume
             </h4>
           </a>
         </Row>
